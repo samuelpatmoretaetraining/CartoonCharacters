044d2f9b86de5885ac022347dacd072c51900858
samuelpatmoretaetraining <samuelpatmoretaetraining@googlemail.com>
2017-12-05 18:40:39 +0000

    cropNames function created to get arraylist of names from API call.

--- app/src/main/java/com/muelpatmore/cartooncharacters/data/network/APIManager.java
+++ app/src/main/java/com/muelpatmore/cartooncharacters/data/network/APIManager.java
@@ -4,6 +4,7 @@ import android.util.Log;
 
 import com.muelpatmore.cartooncharacters.BuildConfig;
 import com.muelpatmore.cartooncharacters.data.event_bus.CharacterListReady;
+import com.muelpatmore.cartooncharacters.data.network.models.CharacterListModel;
 import com.muelpatmore.cartooncharacters.data.network.models.CharacterModel;
 import com.muelpatmore.cartooncharacters.data.network.network_utils.RequestInterface;
 import com.muelpatmore.cartooncharacters.data.network.network_utils.SchedulerProvider;
@@ -12,12 +13,10 @@ import com.muelpatmore.cartooncharacters.data.network.network_utils.ServerConnec
 
 import org.greenrobot.eventbus.EventBus;
 
+
 import java.util.ArrayList;
-import java.util.List;
 
 import io.reactivex.disposables.CompositeDisposable;
-import io.reactivex.functions.Consumer;
-import io.reactivex.schedulers.Schedulers;
 
 /**
  * Created by Samuel on 05/12/2017.
@@ -40,19 +39,45 @@ public class APIManager {
     public void getCharacterList() {
         compositeDisposable.add(
                 request.getCharacterList()
-                    .map(x -> x.getCharacterModels())
-                    .flatMapIterable(x -> x)
-                    .map(x -> x.getText())
-                    .toList()
-                    .observeOn(schedulerProvider.ui())
-                    .subscribeOn(schedulerProvider.io())
-                    .subscribe(characterList -> {
-                        ArrayList<String> characterArrayList = new ArrayList<>(characterList);
-                        for (String s : characterArrayList) {
-                            Log.i(TAG, s);
-                        }
-                        EventBus.getDefault().post(new CharacterListReady(characterArrayList));
-                    }, throwable -> throwable.printStackTrace())
+                        .observeOn(schedulerProvider.ui())
+                        .subscribeOn(schedulerProvider.io())
+                        .subscribe(characterList -> {
+                            cropNames(characterList);
+                        }, throwable -> throwable.printStackTrace())
         );
     }
+
+
+
+    public void cropNames(CharacterListModel characterListModel) {
+        io.reactivex.Observable.just(characterListModel)
+                .map(x -> x.getCharacterModels())
+                .flatMapIterable(x -> x)
+                .map(x -> x.getText())
+                .toList()
+                .subscribe(names -> {
+                        EventBus.getDefault().post(
+                                new CharacterListReady(
+                                        new ArrayList<>(names)));}
+                , throwable -> throwable.printStackTrace());
+    }
+
+//    public void getCharacterList() {
+//        compositeDisposable.add(
+//                request.getCharacterList()
+//                        .map(x -> x.getCharacterModels())
+//                        .flatMapIterable(x -> x)
+//                        .map(x -> x.getText())
+//                        .toList()
+//                        .observeOn(schedulerProvider.ui())
+//                        .subscribeOn(schedulerProvider.io())
+//                        .subscribe(characterList -> {
+//                            ArrayList<String> characterArrayList = new ArrayList<>(characterList);
+//                            for (String s : characterArrayList) {
+//                                Log.i(TAG, s);
+//                            }
+//                            EventBus.getDefault().post(new CharacterListReady(characterArrayList));
+//                        }, throwable -> throwable.printStackTrace())
+//        );
+//    }
 }
