44ae975adace997391f9d9053bf7408aa1f8716e
samuelpatmoretaetraining <samuelpatmoretaetraining@googlemail.com>
2017-12-06 09:54:05 +0000

    Realm initiated in new Application child file.

---
+++ app/src/main/java/com/muelpatmore/cartooncharacters/CartoonCharacterApp.java
@@ -0,0 +1,39 @@
+package com.muelpatmore.cartooncharacters;
+
+import android.app.Application;
+import android.content.Context;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
+/**
+ * Created by Samuel on 06/12/2017.
+ */
+
+public class CartoonCharacterApp extends Application {
+
+    private static Context mContext;
+    private static CartoonCharacterApp instance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+        mContext = this.getApplicationContext();
+
+        Realm.init(mContext);
+        RealmConfiguration realmConfig = new RealmConfiguration.Builder()
+                .schemaVersion(1)
+                .deleteRealmIfMigrationNeeded()
+                .build();
+        Realm.setDefaultConfiguration(realmConfig);
+    }
+
+    public static Context getContext() {
+        return mContext;
+    }
+
+    public static CartoonCharacterApp getInstance() {
+        return instance;
+    }
+}
